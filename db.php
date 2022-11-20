d8258abd1827d2ed22e414c8446af125a0e611e1
Hüseyin YAVUZ <huseyin@nessecloud.com>
2022-11-20 16:59:51 +0300

    s

--- db.php
+++
@@ -1,9 +0,0 @@
-     <?php 
-try {
-     $nesse = new PDO("mysql:host=localhost;dbname=nesse_yeni", "root", "");
-     $nesse->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch ( PDOException $e ){
-     print $e->getMessage();
-}
-     $nesse->query("SET CHARACTER SET utf8"); 
-      ?>
