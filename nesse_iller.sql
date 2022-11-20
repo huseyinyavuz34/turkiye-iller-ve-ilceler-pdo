7420aef3719b85c4b0c3b2f750fe00130c347c1a
Hüseyin YAVUZ <huseyin@nessecloud.com>
2022-11-20 16:56:25 +0300

    Türkiye İller ve İlçeler PDO Veritabanı

---
+++ nesse_iller.sql
@@ -0,0 +1,145 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Anamakine: 127.0.0.1
+-- Üretim Zamanı: 20 Kas 2022, 14:52:20
+-- Sunucu sürümü: 10.4.24-MariaDB
+-- PHP Sürümü: 7.4.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Veritabanı: `nesse_yeni`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `nesse_iller`
+--
+
+CREATE TABLE `nesse_iller` (
+  `id` int(11) NOT NULL,
+  `sehiradi` varchar(255) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Tablo döküm verisi `nesse_iller`
+--
+
+INSERT INTO `nesse_iller` (`id`, `sehiradi`) VALUES
+(1, 'ADANA'),
+(2, 'ADIYAMAN'),
+(3, 'AFYON'),
+(4, 'AĞRI'),
+(5, 'AMASYA'),
+(6, 'ANKARA'),
+(7, 'ANTALYA'),
+(8, 'ARTVİN'),
+(9, 'AYDIN'),
+(10, 'BALIKESİR'),
+(11, 'BİLECİK'),
+(12, 'BİNGÖL'),
+(13, 'BİTLİS'),
+(14, 'BOLU'),
+(15, 'BURDUR'),
+(16, 'BURSA'),
+(17, 'ÇANAKKALE'),
+(18, 'ÇANKIRI'),
+(19, 'ÇORUM'),
+(20, 'DENİZLİ'),
+(21, 'DİYARBAKIR'),
+(22, 'EDİRNE'),
+(23, 'ELAZIĞ'),
+(24, 'ERZİNCAN'),
+(25, 'ERZURUM'),
+(26, 'ESKİŞEHİR'),
+(27, 'GAZİANTEP'),
+(28, 'GİRESUN'),
+(29, 'GÜMÜŞHANE'),
+(30, 'HAKKARİ'),
+(31, 'HATAY'),
+(32, 'ISPARTA'),
+(33, 'İÇEL'),
+(34, 'İSTANBUL'),
+(35, 'İZMİR'),
+(36, 'KARS'),
+(37, 'KASTAMONU'),
+(38, 'KAYSERİ'),
+(39, 'KIRKLARELİ'),
+(40, 'KIRŞEHİR'),
+(41, 'KOCAELİ'),
+(42, 'KONYA'),
+(43, 'KÜTAHYA'),
+(44, 'MALATYA'),
+(45, 'MANİSA'),
+(46, 'KAHRAMANMARAŞ'),
+(47, 'MARDİN'),
+(48, 'MUĞLA'),
+(49, 'MUŞ'),
+(50, 'NEVŞEHİR'),
+(51, 'NİĞDE'),
+(52, 'ORDU'),
+(53, 'RİZE'),
+(54, 'SAKARYA'),
+(55, 'SAMSUN'),
+(56, 'SİİRT'),
+(57, 'SİNOP'),
+(58, 'SİVAS'),
+(59, 'TEKİRDAĞ'),
+(60, 'TOKAT'),
+(61, 'TRABZON'),
+(62, 'TUNCELİ'),
+(63, 'ŞANLIURFA'),
+(64, 'UŞAK'),
+(65, 'VAN'),
+(66, 'YOZGAT'),
+(67, 'ZONGULDAK'),
+(68, 'AKSARAY'),
+(69, 'BAYBURT'),
+(70, 'KARAMAN'),
+(71, 'KIRIKKALE'),
+(72, 'BATMAN'),
+(73, 'ŞIRNAK'),
+(74, 'BARTIN'),
+(75, 'ARDAHAN'),
+(76, 'IĞDIR'),
+(77, 'YALOVA'),
+(78, 'KARABÜK'),
+(79, 'KİLİS'),
+(80, 'OSMANİYE'),
+(81, 'DÜZCE');
+
+--
+-- Dökümü yapılmış tablolar için indeksler
+--
+
+--
+-- Tablo için indeksler `nesse_iller`
+--
+ALTER TABLE `nesse_iller`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Dökümü yapılmış tablolar için AUTO_INCREMENT değeri
+--
+
+--
+-- Tablo için AUTO_INCREMENT değeri `nesse_iller`
+--
+ALTER TABLE `nesse_iller`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=82;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
